d192d2910d20451c93e512de5af947e38def6e01
unknown <bark6256@naver.com>
2021-10-25 14:42:22 +0900

    csv 파일 읽어오기 및 확인

--- fish.py
+++ fish.py
@@ -1,3 +1,20 @@
 import numpy as np
 import pandas as pd
 
+bream_length  = pd.read_csv('C:\\fish\\bream_length.csv')
+bream_weight  = pd.read_csv('C:\\fish\\bream_weight.csv')
+smelt_length  = pd.read_csv('C:\\fish\\smelt_length.csv')
+smelt_weight  = pd.read_csv('C:\\fish\\smelt_weight.csv')
+
+print(type(bream_length))
+print(bream_length)
+print("="*50)
+print(type(bream_weight))
+print(bream_weight)
+print("="*50)
+print(type(smelt_length))
+print(smelt_length)
+print("="*50)
+print(type(smelt_weight))
+print(smelt_weight)
+print("="*50)
