425d3fe69dfd7e4a5c78c4b4dd0c9c0051968f32
unknown <bark6256@naver.com>
2021-10-25 15:28:27 +0900

    데이터 프레임 합치기

--- fish.py
+++ fish.py
@@ -1,20 +1,18 @@
 import numpy as np
 import pandas as pd
 
-bream_length  = pd.read_csv('C:\\fish\\bream_length.csv')
-bream_weight  = pd.read_csv('C:\\fish\\bream_weight.csv')
-smelt_length  = pd.read_csv('C:\\fish\\smelt_length.csv')
-smelt_weight  = pd.read_csv('C:\\fish\\smelt_weight.csv')
+bream_length  = pd.read_csv('C:\\fish\\bream_length.csv', names=['길이'])
+bream_weight  = pd.read_csv('C:\\fish\\bream_weight.csv', names=['무게'])
+smelt_length  = pd.read_csv('C:\\fish\\smelt_length.csv', names=['길이'])
+smelt_weight  = pd.read_csv('C:\\fish\\smelt_weight.csv', names=['무게'])
 
-print(type(bream_length))
-print(bream_length)
+bream_data = pd.concat((bream_length, bream_weight), axis=1)
+print(type(bream_data))
+print(bream_data)
+
+smelt_data = pd.concat((smelt_length, smelt_weight), axis=1)
+print(smelt_data)
 print("="*50)
-print(type(bream_weight))
-print(bream_weight)
-print("="*50)
-print(type(smelt_length))
-print(smelt_length)
-print("="*50)
-print(type(smelt_weight))
-print(smelt_weight)
-print("="*50)
+
+fish_data = pd.concat((bream_data, smelt_data), axis=0, ignore_index=True)
+print(fish_data)
